a3abc7e58ca8d2432983f221d5183ae56588111c
Ravi Wu <raviwu@gmail.com>
2017-10-18 14:00:13 +0800

    Add difference_of_squares.rb

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,44 @@
+# difference_of_squares.rb
+
+class Squares
+  @@cached_squares = {}
+
+  def initialize(number)
+    @numbers = (1..number).to_a
+
+    @number_squares ||= @numbers.map do |num|
+      cached_square_of(num).nil? ? cache_square_of(num, num * num) : cached_square_of(num)
+    end
+  end
+
+  def square_of_sum
+    @square_of_sum ||= sum * sum
+  end
+
+  def sum_of_squares
+    @sum_of_squares ||= @number_squares.sum
+  end
+
+  def difference
+    (square_of_sum - sum_of_squares).abs
+  end
+
+  private
+
+  def cached_square_of(number)
+    @@cached_squares[number]
+  end
+
+  def cache_square_of(number, result)
+    @@cached_squares[number] = result
+    result
+  end
+
+  def sum
+    @sum ||= @numbers.sum
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
