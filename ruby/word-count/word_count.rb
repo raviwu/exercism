e39941cf7e549f1469fb6f83c754d56d2d7fb94f
Ravi Wu <raviwu@gmail.com>
2017-10-23 15:00:37 +0800

    Complete word_count

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Phrase
+  def initialize(sentence)
+    @sentence = sentence
+    @word_count = Hash.new(0)
+
+    check_word_count
+  end
+
+  def word_count
+    @word_count
+  end
+
+  private
+
+  def check_word_count
+    @sentence.downcase.scan(/\w+'*\w+|\d+/).each do |word|
+      @word_count[word] += 1
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
