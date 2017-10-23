4b579a6b4ec137ab17a96286c3ff585901b8d665
Ravi Wu <raviwu@gmail.com>
2017-10-23 17:53:46 +0800

    Complete binary

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,15 @@
+class Binary
+  def self.to_decimal(binary)
+    raise ArgumentError unless /^[0-1]+$/ =~ binary
+
+    bases = binary.chars.reverse
+
+    (0..bases.size - 1)
+      .to_a
+      .inject(0) { |sum, index| sum += bases[index].to_i * (2**index) }
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
