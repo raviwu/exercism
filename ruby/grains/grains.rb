504ae1cdae83fc57c01f21be5784b4b70e01b4d8
Ravi Wu <raviwu@gmail.com>
2017-10-25 16:53:52 +0800

    Refactor Grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,22 +2,17 @@
 
 class Grains
   VALID_RANGE = [*1..64].freeze
-  @@grains = {}
 
   def self.square(number)
     raise ArgumentError unless VALID_RANGE.include? number
 
-    @@grains[number] = 2 ** (number - 1)
-    @@grains[number]
+    2**(number - 1)
   end
 
   def self.total
-    VALID_RANGE.each do |index|
-      next if @@grains.dig(index)
-      @@grains[index] = square(index)
+    VALID_RANGE.reduce(0) do |sum, number|
+      sum + square(number)
     end
-
-    @@grains.values.sum
   end
 end
 
