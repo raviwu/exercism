fad014bb8f7f335273a317feddf6aeafdb1ad098
Ravi Wu <raviwu@gmail.com>
2017-10-25 14:03:40 +0800

    Eliminate .to_a after chunk_while

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -2,8 +2,7 @@ class RunLengthEncoding
   def self.encode(input)
     input
       .chars
-      .chunk_while {|i, j| i == j }
-      .to_a
+      .chunk_while { |i, j| i == j }
       .map { |chunk| [chunk.size, chunk[-1]] }
       .flat_map { |chunk| chunk.drop_while { |c| c == 1 } }
       .join
@@ -12,8 +11,7 @@ class RunLengthEncoding
   def self.decode(input)
     input
       .chars
-      .chunk_while {|i, j| /\d+/ =~ i }
-      .to_a
+      .chunk_while { |i, _| i.match?(/\d+/) }
       .map(&:join)
       .map { |chunk| [decode_chunk_size(chunk[0..-2]), chunk[-1]] }
       .map { |chunk| chunk.last * chunk.first }
