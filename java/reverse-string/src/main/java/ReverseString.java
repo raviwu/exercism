805d37396ba6f883bb23f386ecf590c324e29783
Ravi Wu <raviwu@gmail.com>
2018-05-18 16:34:17 +0800

    Complete reversestring

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,13 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        String reversedString = "";
+
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + inputString.charAt(i);
+        }
+
+        return reversedString;
+    }
+
+}
